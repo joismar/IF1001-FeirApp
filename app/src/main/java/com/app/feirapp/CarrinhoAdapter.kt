eee6c5f57493a1536d5248588c12f4cd929cb02a
Joismar <joismar@outlook.com>
2019-06-29 02:27:08 -0300

    Ajustes no Adapter do CArrinho

--- app/src/main/java/com/app/feirapp/CarrinhoAdapter.kt
+++ app/src/main/java/com/app/feirapp/CarrinhoAdapter.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import java.text.DecimalFormat
@@ -20,6 +21,29 @@ class CarrinhoAdapter(private val produtos: ArrayList<Produto>, private val acti
 
         fun dialog(produtos: ArrayList<Produto>, position: Int, adapter: CarrinhoAdapter, activity: MainActivity) {
             carrinhoView.setOnClickListener {
+                val vi = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+                val dialog = AlertDialog.Builder(itemView.context).create()
+                val inflate = vi.inflate(R.layout.item_dialog, null)
+
+                val butcancel = inflate.findViewById(R.id.botao_cancelar) as Button
+                val butedit = inflate.findViewById(R.id.botao_editar) as Button
+                val butremove = inflate.findViewById(R.id.botao_remover) as Button
+
+                dialog.setView(inflate)
+
+                butcancel.setOnClickListener {
+                    dialog.dismiss()
+                }
+                butedit.setOnClickListener {
+                    TODO()
+                }
+                butremove.setOnClickListener {
+                    activity.deletaProduto(position, adapter)
+                }
+
+                dialog.show()
+
+                /*
                 val dialog = AlertDialog.Builder(itemView.context)
 
                 dialog.setTitle("O que deseja fazer?")
@@ -29,6 +53,7 @@ class CarrinhoAdapter(private val produtos: ArrayList<Produto>, private val acti
                 }
                 dialog.setNegativeButton("Cancelar") { _, _ -> }
                 dialog.create().show()
+                */
             }
         }
     }
